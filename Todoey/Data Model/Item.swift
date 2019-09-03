80fff86d50256ea649641b506dbd3436dac03fca
Olajide Osho <olajideosho1995@gmail.com>
2019-09-03 16:19:54 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by DigitalLab MacOne on 03/09/2019.
+//  Copyright © 2019 Weilai Jishu. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
